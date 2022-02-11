da4cc1a96ce7e2b566ab440ffc99dfa81f35b524
Mlmjocker2021 <99417474+Mlmjocker2021@users.noreply.github.com>
2022-02-11 12:16:40 +0530

    Update motech.py

--- bot/motech.py
+++ bot/motech.py
@@ -1,5 +1,5 @@
 # Update
-MT_BOT_UPDATES = "t.me/Mo_tech_YT"
+MT_BOT_UPDATES = "t.me/+0K7y186Sd084MWU1"
 # Your Main Channel
-BOT_UPDATES = "t.me/Mo_Tech_YT"
+BOT_UPDATES = "t.me/+0K7y186Sd084MWU1"
 
